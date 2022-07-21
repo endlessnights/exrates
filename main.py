43a7bac2851d2babff9f3bae73324157166e64bc
endlessnights <fanoflisataiga@gmail.com>
2022-07-22 00:04:04 +0600

    added flask + output some info in localhost:5000

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import requests as requests
 from pip._internal.network.utils import HEADERS
 from pyquery import PyQuery as pq
 
+from flask import Flask
+
+
 
 conn = sqlite3.connect('ratesdb')
 c = conn.cursor()
@@ -25,7 +28,7 @@ else:
     date = str(date.today())
     print('Курс: ' + str(rate))
     # получаем обменный курс
-    exrate = round((1/float(rate)), 2)
+    exrate = round((1 / float(rate)), 2)
     print('Обменный курс: ' + str(exrate))
     # Проверяем совпадает ли последняя запись с текущим курсом
     presence = (c.execute('SELECT EXISTS(SELECT * FROM exrates WHERE exrate = {} LIMIT 1)'.format(exrate))).fetchone()
@@ -39,3 +42,16 @@ else:
                 date, rate, exrate))
         conn.commit()
         conn.close()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return """Обменный курс: {} <br \>
+    Курс: {}""".format(str(exrate), str(rate))
+
+
+if __name__ == '__main__':
+    app.run()
