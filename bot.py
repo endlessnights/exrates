93fd306e0157e79a06e173f04ddad4817a7f771e
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2023-01-29 22:55:56 +0600

    some changes

--- bot.py
+++ bot.py
@@ -30,20 +30,23 @@ def getmirkurs():
     curdate = datetime.strftime(curdate, '%d %B %Y')
     result = '''Сегодня {}
 Курс от {} - {} тенге за 1 руб'''.format(curdate, ratedate, rate)
-    return result
+    result2 = '''Сегодня {}'''.format(curdate)
+    return result, result2
 
 
 whitelist = [326070831, -1001854765380]
-newexrate = '''Новый обменный курс МИР!
-{}'''.format(getmirkurs())
+# newexrate = '''Новый обменный курс МИР!
+# {}'''.format(getmirkurs())
 
 
 @bot.message_handler(commands=['kursmir'])
 def mirexrate(message):
 
     user = message.chat.id
+    result = getmirkurs()
+    a, b = result
     if user in whitelist:
-        bot.send_message(message.chat.id, text=getmirkurs())
+        bot.send_message(message.chat.id, text=a)
     else:
         print(whitelist)
         bot.send_message(message.chat.id, text='''У вас нет доступа к этому боту, 
@@ -53,18 +56,43 @@ def mirexrate(message):
 
 conn = sqlite3.connect('ratesdb')
 c = conn.cursor()
+c.execute('SELECT * FROM exrates WHERE id=(select max(id) from exrates) ORDER BY id DESC LIMIT 1')
+records = c.fetchall()
+columns = [col[0] for col in c.description]
+rows = [dict(zip(columns, row)) for row in c.fetchall()]
+for row in records:
+    ratedate = row[1]
+    rate = row[3]
+c.close()
+
+conn = sqlite3.connect('ratesdb')
+c = conn.cursor()
 c.execute("SELECT COUNT(*) FROM exrates")
 current_record_count = c.fetchone()[0]
 print(current_record_count)
 conn.close()
+
 with open("linecount.txt", "r") as f:
     previous_record_count = int(f.read())
+with open("exrate.txt", "r") as frate: # test
+    previous_rate = float(frate.read()) # test
 if current_record_count > previous_record_count:
     print("New database entry added")
+    c, d = getmirkurs()
+    print('D:', d)
     for chat in whitelist:
-        bot.send_message(chat_id=chat, text=newexrate)
+        if rate > previous_rate: # test
+            bot.send_message(chat_id=chat, text='''Новый обменный курс МИР!
+{}
+🔺{} тенге за 1 руб'''.format(d, rate)) # test
+        else: # test
+            bot.send_message(chat_id=chat, text='''Новый обменный курс МИР!
+{}
+🔻{} тенге за 1 руб'''.format(d, rate)) # test
     with open("linecount.txt", "w") as f:
         f.write(str(current_record_count))
+    with open("exrate.txt", "w") as frate: # test
+        frate.write(str(rate)) # test
 
 
 bot.polling(none_stop=True)
