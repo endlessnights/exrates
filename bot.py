ecf9bfb9f1024fe762f794ccf76d5188b051fca2
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2023-01-30 15:27:41 +0600

    refactor

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
+import json
 import locale
 import sqlite3
 from datetime import date, datetime
 import telebot
 import time
 
-
-api_token = '5956191624:AAHw268WZP_apCbj9aDUc6wAFE9uyEd4B0o'
+# api_token = '5956191624:AAHw268WZP_apCbj9aDUc6wAFE9uyEd4B0o' # test bot @test239botbot
+api_token = '6173204610:AAEVmFTmk-b3-UdjlUqHFyyFvVbI6va6Ymg'  # production bot @mirexratebot
 bot = telebot.TeleBot(api_token)
 
 
@@ -34,16 +35,21 @@ def getmirkurs():
     return result, result2
 
 
-whitelist = [326070831, -1001854765380]
+# Белый список пользователей и чатов, которые могут пользоваться ботом
+with open("whitelist.txt", "r") as fwl:
+    whiteliststr = str(fwl.read())
+    whitelist = json.loads(whiteliststr)
 
-# print(type(whitelist))
-# newexrate = '''Новый обменный курс МИР!
-# {}'''.format(getmirkurs())
+
+@bot.message_handler(commands=['start'])
+def hellouser(message):
+    bot.send_message(message.chat.id, text='''Привет!
+Доступ к боту платный: 100 тг/мес за пользователя, 2000 тг/мес для группы (все участники могут вызывать бота)
+Чтобы начать работу с ботом, нажмите /kursmir''')
 
 
 @bot.message_handler(commands=['kursmir'])
 def mirexrate(message):
-
     user = message.chat.id
     result = getmirkurs()
     a, b = result
@@ -53,7 +59,7 @@ def mirexrate(message):
         print(whitelist)
         bot.send_message(message.chat.id, text='''У вас нет доступа к этому боту, 
 Если вы считаете, что это ошибка, 
-пожалуйста, сообщите Ваш ID {} администратору боту: https://t.me/pycarrot2'''.format(user))
+пожалуйста, сообщите Ваш ID {} администратору боту: @pycarrot2.'''.format(user))
 
 
 conn = sqlite3.connect('ratesdb')
@@ -65,47 +71,31 @@ rows = [dict(zip(columns, row)) for row in c.fetchall()]
 for row in records:
     ratedate = row[1]
     rate = row[3]
-c.close()
-
-conn = sqlite3.connect('ratesdb')
-c = conn.cursor()
 c.execute("SELECT COUNT(*) FROM exrates")
 current_record_count = c.fetchone()[0]
 print(current_record_count)
-conn.close()
+c.close()
 
 with open("linecount.txt", "r") as f:
     previous_record_count = int(f.read())
-with open("exrate.txt", "r") as frate: # test
-    previous_rate = float(frate.read()) # test
+with open("exrate.txt", "r") as frate:  # test
+    previous_rate = float(frate.read())  # test
 if current_record_count > previous_record_count:
     print("New database entry added")
     c, d = getmirkurs()
     print('D:', d)
     for chat in whitelist:
-        if rate > previous_rate: # test
+        if rate > previous_rate:  # test
             bot.send_message(chat_id=chat, text='''Новый обменный курс МИР!
 {}
-🔺{} тенге за 1 руб'''.format(d, rate)) # test
-        else: # test
+🔺{} тенге за 1 руб'''.format(d, rate))  # test
+        else:  # test
             bot.send_message(chat_id=chat, text='''Новый обменный курс МИР!
 {}
-🔻{} тенге за 1 руб'''.format(d, rate)) # test
+🔻{} тенге за 1 руб'''.format(d, rate))  # test
     with open("linecount.txt", "w") as f:
         f.write(str(current_record_count))
-    with open("exrate.txt", "w") as frate: # test
-        frate.write(str(rate)) # test
-
+    with open("exrate.txt", "w") as frate:  # test
+        frate.write(str(rate))  # test
 
 bot.polling(none_stop=True)
-
-
-# conn = sqlite3.connect('ratesdb')
-# c = conn.cursor()
-# c.execute('SELECT COUNT(*) FROM exrates')
-# previous_record_count = c.fetchone()[0]
-# print(previous_record_count)
-# f = open("linecount.txt", "w")
-# f.write(str(previous_record_count))
-# f.close()
-# c.close()
