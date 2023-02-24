924d9f8d4f733e06b7ecaa65b49b2ea83639beb5
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2023-02-24 15:10:10 +0500

    fixed, later will change

--- bot.py
+++ bot.py
@@ -121,17 +121,17 @@ def mirexrate(message):
     return user
 
 
-@bot.message_handler(func=lambda message: message.text and "/admin" not in message.text)
-def handle_message(message):
-    if not message.chat.id == adminuser:
-        bot.reply_to(message, '''Я тебя не понял. Доступные команды:
-/start - перезапуск бота
-/kursmir - показать текущий обменный курс''')
-    else:
-        bot.reply_to(message, '''Я тебя не понял. Доступные команды:
-/start - перезапуск бота
-/kursmir - показать текущий обменный курс
-/admin - Администрирование бота''')
+# @bot.message_handler(func=lambda message: message.text and "/admin" not in message.text)
+# def handle_message(message):
+#     if not message.chat.id == adminuser:
+#         bot.reply_to(message, '''Я тебя не понял. Доступные команды:
+# /start - перезапуск бота
+# /kursmir - показать текущий обменный курс''')
+#     else:
+#         bot.reply_to(message, '''Я тебя не понял. Доступные команды:
+# /start - перезапуск бота
+# /kursmir - показать текущий обменный курс
+# /admin - Администрирование бота''')
 
 
 adminuser = 326070831
